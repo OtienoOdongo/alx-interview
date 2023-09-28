15f2c9c6f67e6a6f77e61ea834ebdff27cc46733
OtienoOdongo <otabiznake@gmail.com>
2023-09-28 12:24:27 +0000

    pascal triangle in python

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,29 +1,42 @@
 #!/usr/bin/python3
+"""
+pascal triangle question
+written in python
+"""
+
 
 def pascal_triangle(n):
-    # Check if n is less than or equal to 0, return an empty list
+    """
+    A function that returns a list of integers n
+    that represents pascal triangle
+    """
+
+    """Checking if n is less than or equal to 0, return an empty list"""
     if n <= 0:
         return []
-    
-    # Initialize a list to store Pascal's triangle
+
+    """Initializing a list to store Pascal's triangle"""
     pascal_t = []
-    
-    # Loop through each row of Pascal's triangle
+
+    """Loop through each row of Pascal's triangle"""
     for i in range(n):
-        # Initialize an empty list for the current row
+        """Initializing an empty list for the current row"""
         row = []
-        
-        # Loop through each position in the current row
+
+        """Loop through each position in the current row"""
         for j in range(i + 1):
-            # If j is the first or last element in the row, it's always 1
+            """If j is the first or last element in the row, it's always 1"""
             if j == 0 or j == i:
                 row.append(1)
             else:
-                # Calculate the value based on the values in the previous row
+                """
+                Calculating the values
+                based on the values in the previous row
+                """
                 row.append(pascal_t[i - 1][j - 1] + pascal_t[i - 1][j])
-        
-        # Append the current row to Pascal's triangle
+
+        """Appending the current row to Pascal's triangle"""
         pascal_t.append(row)
-    
-    # Return the Pascal's triangle as a list of lists
+
+    """Return the Pascal's triangle as a list of lists"""
     return pascal_t
